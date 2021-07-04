0018ed947badc2d6b34a29a6ba1e57c7fc58485b
Luaw92e <85026176+Luaw92e@users.noreply.github.com>
2021-07-04 17:14:13 +0900

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,23 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+client.run("ODU4NTUwMzE0MDgwMjcyNDA0.YNfxSQ.YE6uWqP9zYW3_u71HjJhjI0gTuM")
